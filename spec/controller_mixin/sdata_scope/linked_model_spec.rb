c1f252432dc34b87b71ca73740a02d7d614b53d2
DanielVartanov <daniel.vartanov@gmail.com>
2010-10-13 17:52:17 +0600

    linked_model_spec is totally green now (thanks to sdata_application, which is moved into plugin now)

--- spec/controller_mixin/sdata_scope/linked_model_spec.rb
+++ spec/controller_mixin/sdata_scope/linked_model_spec.rb
@@ -1,24 +1,29 @@
 require File.join(File.dirname(__FILE__), '..', '..', 'spec_helper')
 
 describe SData::ControllerMixin, "#sdata_scope" do
-  context "given a controller which acts as sdata and accesses a linked model" do
+  context "given linked model resource" do
     context "being configured without user scoping" do
       before :all do
+        Object.__send__ :remove_const, :SomeResource if defined?(SomeResource)
+
+        class SDataApplication
+          include SData::ControllerMixin::Actions
+          include SData::Application::ResourceFromParams
+          include SData::Application::OldDslSupport
+        end
+
+        @application = SDataApplication.new
+        
+
         BaseModel = Class.new(ActiveRecord::Base)
 
-        Object.__send__ :remove_const, :Model if defined?(Model)
-        class Model < SData::Resource::Base
+        class SomeResource < SData::Resource::Base
           self.baze_class = BaseModel
 
           define_payload_map :born_at => { :baze_field => :born_at }
 
-          acts_as_sdata :link => :simply_guid
-        end
-
-        class Controller < ActionController::Base
-          extend SData::ControllerMixin
-
-          acts_as_sdata  :model => Model, :feed =>
+          has_sdata_options :link => :simply_guid,
+                            :feed =>
                            {:author => 'Billing Boss',
                             :path => '/trading_accounts',
                             :title => 'Billing Boss | Trading Accounts',
@@ -28,29 +33,29 @@ describe SData::ControllerMixin, "#sdata_scope" do
       end
 
       before :each do
-        @controller = Controller.new
-        Model.stub! :all => []
+        @resource = SomeResource.new
+        SomeResource.stub! :all => []
       end
 
       context "when params contain :condition key and where clause" do
         before :each do
-          @controller.stub! :params => { 'where born_at gt 1900' => nil, :condition => '$linked' }
+          @application.stub! :params => { 'where born_at gt 1900' => nil, :condition => '$linked', :sdata_resource => 'SomeResource' }
         end
 
         it "should apply to SData::Predicate for conditions and append requirement for simply guid" do
-          BaseModel.should_receive(:find_with_deleted).with(:all, {:conditions => ['"born_at" > ? and id IN (SELECT bb_model_id FROM sd_uuids WHERE bb_model_type = \'BaseModel\' and sd_class = \'Model\')', '1900']}).and_return([])
-          @controller.send :sdata_scope
+          BaseModel.should_receive(:find_with_deleted).with(:all, {:conditions => ['"born_at" > ? and id IN (SELECT bb_model_id FROM sd_uuids WHERE bb_model_type = \'BaseModel\' and sd_class = \'SomeResource\')', '1900']}).and_return([])
+          @application.send :sdata_scope
         end
       end
 
       context "when params contain :condition key but does not contain where clause" do
         before :each do
-          @controller.stub! :params => {:condition => '$linked'}
+          @application.stub! :params => {:condition => '$linked', :sdata_resource => 'SomeResource'}
         end
 
         it "should return all entity records with simply guid" do
-          BaseModel.should_receive(:find_with_deleted).with(:all, {:conditions => ['id IN (SELECT bb_model_id FROM sd_uuids WHERE bb_model_type = \'BaseModel\' and sd_class = \'Model\')']}).and_return([])
-          @controller.send :sdata_scope
+          BaseModel.should_receive(:find_with_deleted).with(:all, {:conditions => ['id IN (SELECT bb_model_id FROM sd_uuids WHERE bb_model_type = \'BaseModel\' and sd_class = \'SomeResource\')']}).and_return([])
+          @application.send :sdata_scope
         end
       end
     end
