589e1bc8e3a1cca1d8b84b74cddc9ead221eabd2
DanielVartanov <daniel.vartanov@gmail.com>
2010-10-14 21:06:55 +0600

    linked_model_spec: switched to new DSL and made it green

--- spec/controller_mixin/sdata_scope/linked_model_spec.rb
+++ spec/controller_mixin/sdata_scope/linked_model_spec.rb
@@ -1,33 +1,29 @@
 require File.join(File.dirname(__FILE__), '..', '..', 'spec_helper')
 
-describe SData::ControllerMixin, "#sdata_scope" do
+describe SData::Application, "#sdata_scope" do
   context "given linked model resource" do
     context "being configured without user scoping" do
       before :all do
-        Object.__send__ :remove_const, :SomeResource if defined?(SomeResource)
-
-        @application = SData::TestApplication.new
-
         BaseModel = Class.new(ActiveRecord::Base)
 
+        Object.__send__ :remove_const, :SomeResource if defined?(SomeResource)
         class SomeResource < SData::Resource::Base
           self.baze_class = BaseModel
 
           define_payload_map :born_at => { :baze_field => :born_at }
 
           has_sdata_options :link => :simply_guid,
-                            :feed =>
-                           {:author => 'Billing Boss',
-                            :path => '/trading_accounts',
-                            :title => 'Billing Boss | Trading Accounts',
-                            :default_items_per_page => 10,
-                            :maximum_items_per_page => 100}
+            :feed =>
+              { :author => 'Billing Boss',
+              :path => '/trading_accounts',
+              :title => 'Billing Boss | Trading Accounts',
+              :default_items_per_page => 10,
+              :maximum_items_per_page => 100 }
         end
-      end
 
-      before :each do
-        @resource = SomeResource.new
         SomeResource.stub! :all => []
+
+        @application = SData::TestApplication.new
       end
 
       context "when params contain :condition key and where clause" do
