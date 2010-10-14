1e9cc7f7a2b00eae05661c1e0fe2a84cf439309d
DanielVartanov <daniel.vartanov@gmail.com>
2010-10-14 21:14:21 +0600

    scope_in_config_spec: understands new sctructure now

--- spec/controller_mixin/sdata_scope/scoping_in_config_spec.rb
+++ spec/controller_mixin/sdata_scope/scoping_in_config_spec.rb
@@ -1,62 +1,54 @@
 require File.join(File.dirname(__FILE__), '..', '..', 'spec_helper')
 
-describe SData::ControllerMixin, "#sdata_scope" do
-  context "given a controller which acts as sdata and accesses a linked model" do
+describe SData::Application, "#sdata_scope" do
+  context "given a resource and trying to access a linked model" do
     context "being configured with user scoping" do
       before :all do
         BaseModel = Class.new(ActiveRecord::Base)
 
-        Object.__send__ :remove_const, :Model if defined?(Model)
-        class Model < SData::Resource::Base
+        Object.__send__ :remove_const, :SomeResource if defined?(SomeResource)
+
+        class SomeResource < SData::Resource::Base
           self.baze_class = BaseModel
 
           define_payload_map :born_at => { :baze_field => :born_at }
 
-          acts_as_sdata :link => :simply_guid
+          has_sdata_options :link => :simply_guid,
+            :feed =>
+              { :author => 'Billing Boss',
+              :path => '/trading_accounts',
+              :title => 'Billing Boss | Trading Accounts',
+              :default_items_per_page => 10,
+              :maximum_items_per_page => 100 },
+            :scoping => ["created_by_id = ?"]
         end
+        SomeResource.stub! :all => []
 
-        class Controller < ActionController::Base
-          extend SData::ControllerMixin
-
-          acts_as_sdata  :model => Model, :feed =>
-                           {:author => 'Billing Boss',
-                            :path => '/trading_accounts',
-                            :title => 'Billing Boss | Trading Accounts',
-                            :default_items_per_page => 10,
-                            :maximum_items_per_page => 100},
-                            :scoping => ["created_by_id = ?"]
-        end
-      end
-
-      before :each do
+        @app = SData::TestApplication.new
         @user = User.new.populate_defaults
-        @controller = Controller.new
-        Model.stub! :all => []
+        @app.stub! :current_user => @user
+        @app.stub! :target_user => @user
       end
 
       context "with no other params" do
         before :each do
-          @controller.stub! :params => {}
-          @controller.stub! :current_user => @user
-          @controller.stub! :target_user => @user
+          @app.stub! :params => { :sdata_resource => 'SomeResource' }
         end
 
         it "should return all entity records created_by scope" do
-          Model.should_receive(:all).with :conditions => ['created_by_id = ?', "#{@user.id}"]
-          @controller.send :sdata_scope
+          SomeResource.should_receive(:all).with :conditions => ['created_by_id = ?', "#{@user.id}"]
+          @app.send :sdata_scope
         end
       end
 
       context "with condition and where clause" do
         before :each do
-          @controller.stub! :params => { 'where born_at gt 1900' => nil, :condition => '$linked' }
-          @controller.stub! :current_user => @user
-          @controller.stub! :target_user => @user
+          @app.stub! :params => { 'where born_at gt 1900' => nil, :condition => '$linked', :sdata_resource => 'SomeResource' }
         end
 
         it "should return all entity records with created_by, predicate, and link scope" do
-          BaseModel.should_receive(:find_with_deleted).with(:all, {:conditions => ['"born_at" > ? and created_by_id = ? and id IN (SELECT bb_model_id FROM sd_uuids WHERE bb_model_type = \'BaseModel\' and sd_class = \'Model\')', '1900', @user.id.to_s]}).and_return([])
-          @controller.send :sdata_scope
+          BaseModel.should_receive(:find_with_deleted).with(:all, {:conditions => ['"born_at" > ? and created_by_id = ? and id IN (SELECT bb_model_id FROM sd_uuids WHERE bb_model_type = \'BaseModel\' and sd_class = \'SomeResource\')', '1900', @user.id.to_s]}).and_return([])
+          @app.send :sdata_scope
         end
       end
     end
