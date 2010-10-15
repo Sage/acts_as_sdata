f6768f8023d8154c0ae254a282eb3a8971e7938b
DanielVartanov <daniel.vartanov@gmail.com>
2010-10-15 22:24:23 +0600

    find_by_sdata_instance_id_spec fixed

--- spec/resource/find_by_sdata_instance_id_spec.rb
+++ spec/resource/find_by_sdata_instance_id_spec.rb
@@ -3,30 +3,36 @@ require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 describe SData::Resource::Base, "#find_by_sdata_instance_id" do
   describe "given an SData::Resource::Base derivative" do
     before :all do
-      class Model < SData::Resource::Base; end
+      BaseModel = Class.new(ActiveRecord::Base)
     end
 
     describe "when sdata_options contain :instance_id" do
-      before :each do
-        Model.class_eval { has_sdata_options :instance_id => :email }
+      before :all do
+        class ResourceWithInstanceId < SData::Resource::Base;
+          has_sdata_options :instance_id => :email
+
+          self.baze_class = BaseModel
+        end
       end
 
       it "should find by a field assigned to :instance_id option value" do
         email = "e@ma.il"
-        Model.should_receive(:find).with(:first, :conditions => { :email => email }).once
-        Model.find_by_sdata_instance_id(email)
+        BaseModel.should_receive(:find).with(:first, :conditions => { :email => email }).once
+        ResourceWithInstanceId.find_by_sdata_instance_id(email)
       end
     end
 
     describe "when sdata_options does not contain :instance_id" do
-      before :each do
-        Model.class_eval { has_sdata_options }
+      before :all do
+        class ResourceWithoutInstanceId < SData::Resource::Base;
+          self.baze_class = BaseModel
+        end
       end
 
       it "should consider :id as SData instance ID" do
         id = 1
-        Model.should_receive(:find).with(id).once
-        Model.find_by_sdata_instance_id(id)
+        BaseModel.should_receive(:find).with(id).once
+        ResourceWithoutInstanceId.find_by_sdata_instance_id(id)
       end
     end
   end
