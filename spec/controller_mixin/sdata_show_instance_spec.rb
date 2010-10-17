4468bbffb54ab017d8a12968e1ad36a11cbe9971
DanielVartanov <daniel.vartanov@gmail.com>
2010-10-17 21:15:20 +0600

    sdata_show_instance_spec fixed (except missing bookkeeper problem)

--- spec/controller_mixin/sdata_show_instance_spec.rb
+++ spec/controller_mixin/sdata_show_instance_spec.rb
@@ -1,46 +1,39 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
-include SData
-
-describe ControllerMixin, "#sdata_show_instance" do
-  module Sage
-    module BusinessLogic
-      module Exception
-        class AccessDeniedException < StandardError
-          
-        end
+describe SData::Application, "#sdata_show_instance" do
+  before :all do
+    module Sage
+      module BusinessLogic
+        class ExceptionAccessDeniedException < StandardError; end
       end
     end
   end
-  describe "given a controller which acts as sdata" do
+
+  describe "given a SData application which acts as sdata" do
     before :all do
-     Base = Class.new(ActionController::Base)
-     Base.extend ControllerMixin
+      @application = SData::TestApplication.new
     end
+
     describe "given a model without baze" do
       before :all do
-        Model = Class.new
-        Base.acts_as_sdata  :model => Model
+        Object.__send__ :remove_const, :SomeResource if defined?(SomeResource)
+        class SomeResource < SData::Resource::Base; end;
       end
-  
-      before :each do
-        @controller = Base.new
-      end
-  
+
       describe "when params contain :instance_id key" do
         before :each do
           @instance_id = 1
-          @controller.stub! :params => { :instance_id => @instance_id },
+          @application.stub! :params => { :instance_id => @instance_id, :sdata_resource => 'SomeResource' },
                             :current_user => OpenStruct.new(:sage_username => 'bob', :id => 1),
                             :target_user => OpenStruct.new(:sage_username => 'bob', :id => 1),
                             :logged_in? => true
         end
-  
+
 #        describe "when record with such id exists and belongs to user" do
 #          before :each do
-#            @record = Model.new
+#            @record = SomeResource.new
 #            @record.stub! :owner => OpenStruct.new(:sage_username => 'bob', :id => 1)
-#            Model.should_receive(:find_by_sdata_instance_id).with(@instance_id).and_return(@record)
+#            SomeResource.should_receive(:find_by_sdata_instance_id).with(@instance_id).and_return(@record)
 #          end
 #  
 #          it "should render atom entry of the record" do
@@ -53,22 +46,22 @@ describe ControllerMixin, "#sdata_show_instance" do
 
         describe "when record with such id exists but does not belong to user" do
           before :each do
-            @record = Model.new
+            @record = SomeResource.new
             @record.stub! :owner => OpenStruct.new(:sage_username => 'mary', :id => 2)
-            Model.should_receive(:find_by_sdata_instance_id).with(@instance_id).and_return(@record)
+            SomeResource.should_receive(:find_by_sdata_instance_id).with(@instance_id).and_return(@record)
           end
   
           it "should render atom entry of the record" do
             # Can't get raise_error to work properly :/
             entry = Atom::Entry.new
-            @controller.should_receive(:handle_exception)
-            @controller.sdata_show_instance
+            @application.should_receive(:handle_exception)
+            @application.sdata_show_instance
           end
         end
   
         describe "when record with such id does not exist" do
           before :each do
-            Model.should_receive(:find_by_sdata_instance_id).with(@instance_id).and_return(nil)
+            SomeResource.should_receive(:find_by_sdata_instance_id).with(@instance_id).and_return(nil)
           end
   
           it "should..." do
@@ -79,7 +72,7 @@ describe ControllerMixin, "#sdata_show_instance" do
   
       describe "whem params does not contain :instance_id key" do
         before :each do
-          @controller.stub! :params => Hash.new
+          @application.stub! :params => Hash.new
         end
   
         it "should..." do
@@ -92,7 +85,6 @@ describe ControllerMixin, "#sdata_show_instance" do
       it "should ..." do
         pending "not yet tested"
       end
-
     end
   end
 end
