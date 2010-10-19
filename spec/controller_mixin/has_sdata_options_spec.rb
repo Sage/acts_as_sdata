83a7dccbdf8e46181144ee2578ffd61bbf19ea87
DanielVartanov <daniel.vartanov@gmail.com>
2010-10-19 14:34:35 +0600

    has_sdata_options_spec is fixed

--- spec/controller_mixin/has_sdata_options_spec.rb
+++ spec/controller_mixin/has_sdata_options_spec.rb
@@ -1,25 +1,24 @@
 __DIR__ = File.dirname(__FILE__)
 require File.join(__DIR__, '..', 'spec_helper')
 
-include SData
-
-describe Resource::Base, ".has_sdata_options" do
+describe SData::Resource::Base, ".has_sdata_options" do
   describe "given an sdata resource" do
     before :all do
-      Base = Class.new Resource::Base
+      Object.__send__ :remove_const, :SomeResource if defined?(SomeResource)
+      class SomeResource < SData::Resource::Base; end
     end
 
     before :each do
       @options = { :model => Class.new }
-      Base.has_sdataoptions @options
+      SomeResource.has_sdata_options @options
     end
 
     it "should make passed options available for class" do
-      Base.sdata_options.should == @options
+      SomeResource.sdata_options.should == @options
     end
 
     it "should make passed options available for instances" do
-      Base.new.sdata_options.should == @options
+      SomeResource.new.sdata_options.should == @options
     end
   end
 end
