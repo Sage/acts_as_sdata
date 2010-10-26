75ae0b0474f027fd4ca8b0578ba92da18b119cff
DanielVartanov <daniel.vartanov@gmail.com>
2010-10-26 16:11:19 +0600

    Specs for SData::Collection::Feed. Cases: healthy entries, erroneous payloads

--- spec/collection/feed_spec.rb
+++ spec/collection/feed_spec.rb
@@ -1,12 +1,6 @@
-require File.join(File.dirname(__FILE__),  'spec_helper')
+require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe SData::Collection::Feed do
-  class ErroneousEntry < Customer
-    def to_xml
-      raise "Something went wrong"
-    end    
-  end  
-
   before do
     @feed_options = { :id => 'some-unique-id',
             :author => 'Test Author',
@@ -14,21 +8,124 @@ describe SData::Collection::Feed do
             :title => 'List of Test Items',
             :default_items_per_page => 10,
             :maximum_items_per_page => 100 }
+    @resource_url = Customer.sdata_resource_kind_url('-')
+  end
+
+  def build_feed(entries)
+    SData::Collection::Feed.new(entries, @resource_url, @feed_options)
+  end
+
+  def feed_xml
+    parse_xml(@feed.to_xml)
+  end
+
+  shared_examples_for "any SData feed" do
+    it "should be an Atom Feed" do
+      feed_xml.should have_xpath('/xmlns:feed[@xmlns="http://www.w3.org/2005/Atom"]')
+    end
+
+    it "should respect given feed options"
+
+    # it "should include given links"
+
+    # it "should include given categories"
   end
 
   context "when all entries are healthy" do
-    subject { SData::Collection::Feed.new([Customer.new, Customer.new], @feed_options) }
+    before { @feed = build_feed [Customer.new, Customer.new] }
 
-    it "should be a regular Atom Feed with two entries"
-  end
+    it_should_behave_like "any SData feed"
 
-  context "when some of entries are erroneous" do
-    subject { SData::Collection::Feed.new([Customer.new, ErroneousEntry.new], @feed_options) }
+    it "should contain two entries" do
+      feed_xml.xpath('/xmlns:feed/entry').should have(2).entries
+    end
 
-    
+    it "should not contain eny diagnoses" do
+      feed_xml.should_not have_xpath("//xmlns:entry/sdata:diagnosis")
+    end
   end
 
   context "when there is no entries" do
-    subject { SData::Collection::Feed.new([], @feed_options) }
+    subject { @feed = build_feed [] }
+
+    it_should_behave_like "any SData feed"
+
+    it "should have no entries"
+
+    it "should have no diagnoses"
+  end
+
+  context "when entry payload is erroneous" do
+    class CustomerWithErroneousPayload < Customer
+      def resource_header_attributes
+        raise "Something went wrong"
+      end
+    end
+
+    before { @feed = build_feed [Customer.new, CustomerWithErroneousPayload.new] }
+
+    it_should_behave_like "any SData feed"
+
+    it "should contain all scope elements as entries" do
+      feed_xml.xpath('/xmlns:feed/entry').should have(2).entries
+    end
+
+    describe "erroneous entry" do
+      subject { feed_xml.xpath('/xmlns:feed/xmlns:entry').first }
+
+      it "should contain SData diagnosis" do
+        subject.should have_xpath('sdata:diagnosis')
+      end
+
+      describe "entry diagnosis" do
+        subject { feed_xml.xpath('//xmlns:entry/sdata:diagnosis').first }
+
+        it "should have 'error' severity" do
+          subject.xpath('sdata:severity/text()').to_s.should == 'error'
+        end
+
+        it "should contain diagnosis type in sdata:sdataCode node" do
+          subject.xpath('sdata:sdataCode/text()').to_s.should == 'ApplicationDiagnosi'
+        end
+
+        it "should contain actual exception message in sdata:message node" do
+          subject.xpath('sdata:message/text()').to_s.should == "Exception while trying to construct payload map"
+        end
+
+        it "should include stacktrace" do
+          subject.xpath('sdata:stackTrace/text()').to_s.should include('/collection_spec.rb')
+        end
+      end
+    end
+
+    describe "healthy entry" do
+      subject { feed_xml.xpath('/xmlns:feed/xmlns:entry').second }
+
+      it "should not contain SData diagnosis" do
+        subject.should_not have_xpath('sdata:diagnosis')
+      end
+
+      it "should have non-empty id" do
+        subject.xpath('xmlns:id/text()').to_s.should_not be_empty
+      end
+
+      it "should have non-empty content" do
+        subject.xpath('xmlns:content/text()').to_s.should_not be_empty
+      end
+
+      it "should have SData payload" do
+        subject.should have_xpath('sdata:payload')
+      end
+    end
+  end
+
+  context "when the whole entry is erroneous" do
+    class ErroneousCustomer < Customer
+      def id
+        raise "Something went wrong"
+      end
+    end
+
+    it_should_behave_like "any SData feed"
   end
 end
