872d0bcf70167133efa876fc2d4d11051628639f
DanielVartanov <daniel.vartanov@gmail.com>
2010-10-27 20:29:37 +0600

    Added RDoc to #raised_exception, because its name does not explains its purpose well

--- spec/spec_helpers/raised_exception.rb
+++ spec/spec_helpers/raised_exception.rb
@@ -1,5 +1,8 @@
-# Use this method instead of just ExceptionClass.new. It adds non-nil
-# backtrace and other properties
+# call-seq:
+#   raised_exception(RuntimeError, 'Big Bada Boom') => #<RuntimeError:Big Bada Boom>
+#
+# Use this method instead of just ExceptionClass.new.
+# It adds non-nil backtrace and other necessary properties
 
 def raised_exception(exception_class, message)
   begin
