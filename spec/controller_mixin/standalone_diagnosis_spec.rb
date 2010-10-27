9f7a200afbb0efe2f7b94b7302900d0ed4c48b9f
DanielVartanov <daniel.vartanov@gmail.com>
2010-10-27 20:04:48 +0600

    Made standalone_diagnosis_spec. fixesrm 56

--- spec/controller_mixin/standalone_diagnosis_spec.rb
+++ spec/controller_mixin/standalone_diagnosis_spec.rb
@@ -1,20 +1,43 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe SData::Diagnosis do
-  context "when exception is raised at the action method level" do
+  context "when exception is raised at the action method level" do    
     before do
-      @exception = raised_exception(RuntimeError, "Something went wrong")
-      @diagnosis = SData::ApplicationDiagnosis.new(:exception => @exception)
+      exception = raised_exception(RuntimeError, "Something went wrong")
+      standalone_diagnosis = SData::ApplicationDiagnosis.new(:exception => exception)
+      @diagnosis_xml = parse_xml(standalone_diagnosis.to_xml.to_s)
     end
 
-    it "should produce correct XML output" do
-      xml = parse_xml(@diagnosis.to_xml.to_s)
-      xml.xpath('/sdata:diagnoses/sdata:diagnosis').count.should == 1
-      diagnosis_xml = xml.xpath('/sdata:diagnoses/sdata:diagnosis').first
-      diagnosis_xml.xpath('./node()').map(&:name_with_ns).to_set.should == ["sdata:message", "sdata:sdataCode", "sdata:severity", "sdata:stackTrace"].to_set
-      diagnosis_xml.xpath("sdata:message/text()").to_s.should == "Something went wrong"
-      diagnosis_xml.xpath("sdata:sdataCode/text()").to_s.should == "ApplicationDiagnosis"
-      diagnosis_xml.xpath("sdata:severity/text()").to_s.should == "error"
+    describe "XML output of a diagnosis" do
+      subject { @diagnosis_xml }
+
+      it "should have diagnoses container as a root node" do
+        subject.xpath('/sdata:diagnoses').should_not be_empty
+      end
+
+      it "should contain sdata:diagnosis node" do
+        subject.xpath('/sdata:diagnoses/sdata:diagnosis').should_not be_empty
+      end
+
+      describe "sdata:diagnosis node" do
+        subject { @diagnosis_xml.xpath('/sdata:diagnoses/sdata:diagnosis')  }
+
+        it "should fill sdata:message with exception message" do
+          subject.xpath('sdata:message/text()').to_s.should == "Something went wrong"
+        end
+
+        it "should fill sdat:sdataCode with a exception class" do
+          subject.xpath('sdata:sdataCode/text()').to_s.should == "ApplicationDiagnosis"
+        end
+
+        it "should fill sdata:stackTrace with exception stacktrace" do
+          subject.xpath('sdata:stackTrace/text()').to_s.should include('standalone_diagnosis_spec.rb')
+        end
+
+        it "should set 'error' severity" do
+          subject.xpath("sdata:severity/text()").to_s.should == "error"
+        end
+      end
     end
   end
 end
