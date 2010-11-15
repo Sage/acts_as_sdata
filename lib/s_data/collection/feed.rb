fd19e54af1ca190581f56c55b443c7c3d8dd8bc6
DanielVartanov <daniel.vartanov@gmail.com>
2010-11-15 16:11:14 +0600

    A nicer SData::Feed (also working, except links and opensearch parts)

--- lib/s_data/collection/feed.rb
+++ lib/s_data/collection/feed.rb
@@ -1,28 +1,37 @@
 # -*- coding: utf-8 -*-
 module SData
   class Collection
-    class Feed      
-      attr_accessor :resource_url, :options
+    class Feed < Struct.new(:resource_class, :dataset, :feed_options, :entries)
+      def initialize(*args)
+        super(args)
 
-      def initialize(resource_class, entries, resource_url, feed_options)
-        self.atom_feed = Atom::Feed.new
+        build_resource_url
+        build_atom_feed
 
-        class << self.atom_feed
-          include AtomFeedExtensions
-        end        
-
-        self.options = feed_options
-
-        atom_feed.set_properties(resource_class, resource_url, options)
+        atom_feed.set_properties(resource_class, resource_url, feed_options)
         atom_feed.assign_entries(entries)
-        #atom_feed.populate_open_search
-        #atom_feed.build_feed_links
+#        atom_feed.populate_open_search
+ #       atom_feed.build_feed_links
       end
 
       def to_xml
         atom_feed.to_xml
       end
- 
+
+      protected
+
+      def build_resource_url
+        self.resource_url = Customer.sdata_resource_kind_url(dataset)
+      end
+
+      def build_atom_feed
+        self.atom_feed = Atom::Feed.new.tap do |atom_feed|
+          class << atom_feed
+            include AtomFeedExtensions
+          end
+        end
+      end
+
       module AtomFeedExtensions
         attr_accessor :resource_class
         
@@ -94,31 +103,6 @@ module SData
           end
         end
 
-        def params
-          { :count => 10 }
-        end
-        
-        
-
-        # pagination as a separate class?
-        def records_to_return
-          default_items_per_page = options[:default_items_per_page] || 10
-          maximum_items_per_page = options[:maximum_items_per_page] || 100
-          #check whether the count param is castable into integer
-          return default_items_per_page if params[:count].blank? or (params[:count].to_i.to_s != params[:count])
-          items_per_page = [params[:count].to_i, maximum_items_per_page].min
-          items_per_page = default_items_per_page if (items_per_page < 0)
-          items_per_page
-        end
-
-        def one_based_start_index
-          [(params[:startIndex].to_i), 1].max
-        end
-
-        def zero_based_start_index
-          [(one_based_start_index - 1), 0].max
-        end
-
         def populate_open_search
           self[SData.config[:schemas]['opensearch'], 'totalResults'] << @total_results
           self[SData.config[:schemas]['opensearch'], 'startIndex'] << one_based_start_index
@@ -133,6 +117,7 @@ module SData
       protected
 
       attr_accessor :atom_feed
+      attr_accessor :resource_url
     end
   end
 end
