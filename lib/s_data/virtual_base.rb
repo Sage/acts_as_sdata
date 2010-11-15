6c4859a9f8c4a660177ba14f27b0617d611cd843
DanielVartanov <daniel.vartanov@gmail.com>
2010-11-15 16:09:33 +0600

    Merged VirtualBase from optimization branch

--- lib/s_data/virtual_base.rb
+++ lib/s_data/virtual_base.rb
@@ -1,4 +1,33 @@
 module SData
+  class ScopedVirtualBase
+    attr_accessor :scope, :virtual_base_class
+
+    def initialize(scope, virtual_base_class)
+      @scope = scope
+      @virtual_base_class = virtual_base_class
+    end
+
+    def find(*params)
+      virtual_base_class.new(scope.find(*params))
+    end
+
+    def count(*params)
+      scope.count(*params)
+    end
+
+    def first(*params)
+      virtual_base_class.new(scope.first(*params))
+    end
+
+    def all(*params)
+      virtual_base_class.collection(scope.all(*params))
+    end
+    
+    def all_with_deleted(*params)
+      virtual_base_class.collection(scope.find_with_deleted(:all, *params))
+    end
+  end
+  
   module Traits
     VirtualBase = Trait.new do
       extend Forwardable
@@ -40,7 +69,7 @@ module SData
         klass = baze.class
         while(!klass.descends_from_active_record?) do; klass = klass.superclass; end
         klass.name.demodulize
-       end
+      end
 
       def reference
         # GCRM requires reference to be present and unique to each GCRM model. Decided to use baze id, but
@@ -64,7 +93,7 @@ module SData
         if data.is_a? Array
           data.map {|item| virtual_base_for_object(item, the_type) }.compact
         else
-         virtual_base_for_object(data, the_type)
+          virtual_base_for_object(data, the_type)
         end
       end
 
@@ -136,6 +165,72 @@ module SData
           else
             obj
           end
+        end
+      end
+
+      extend Scoping
+
+      module Scoping
+        def with_pagination(offset, records_to_return, &block)
+          if offset.nil?        
+            yield self
+          else
+            options = {:offset => offset, :limit => records_to_return}
+            self.baze_class.with_scope(:find => options) do
+              yield self
+            end
+          end
+        end
+
+        def with_conditions(conditions, &block)
+          options = {:conditions => conditions}
+          self.baze_class.with_scope(:find => options) do
+            yield self
+          end      
+        end
+        
+        def with_predicate(raw_predicate, &block)
+          if raw_predicate.nil?
+            yield self
+          else
+            predicate = SData::Predicate.parse(self.payload_map.baze_fields, raw_predicate)
+            yield self.with_conditions(predicate.to_conditions, &block)
+          end        
+        end
+        
+        # TODO: rename bb_model_id and bb_model_type to model_id and model_type
+        def with_linking(linking, uuid=nil)
+          if linking
+            uuid_clause = uuid.nil? ? '' : "uuid = '#{Predicate.strip_quotes(uuid)}' and "
+            tablename = self.baze_class_name.tableize
+            with_conditions("#{tablename}.id IN (SELECT bb_model_id FROM sd_uuids WHERE #{uuid_clause}(bb_model_type = '#{baze_class_name}') and (sd_class = '#{sdata_name}'))") do
+              yield self
+            end
+          else
+            yield self
+          end
+        end
+        
+        def sdata_scope_for_context(context)
+          has_sdata_scope = self.baze_class.respond_to?(:sdata_scope_for_context)
+          if SData.enforce_scoping?
+            unless has_sdata_scope
+              raise SData::Exceptions::VirtualBase::MissingScope.new("#{self.name}: missing sdata_scope_for_context for #{self.baze_class.name}")
+            end
+          end
+          scope = has_sdata_scope ? self.baze_class.sdata_scope_for_context(context) : self.baze_class
+          ScopedVirtualBase.new(scope, self)
+        end
+
+        def with_baze(active_record_instance, &block)
+          self.with_baze_class(active_record_instance.class, &block)
+        end
+        
+        # RADAR: not threadsafe
+        def with_baze_class(class_or_scope)
+          old_baze_class, self.baze_class = self.baze_class, class_or_scope
+          yield
+          self.baze_class = old_baze_class
         end
       end
     end
