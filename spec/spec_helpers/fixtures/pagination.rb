104d38057cb2fd0c3561581a01b133357a76694d
DanielVartanov <daniel.vartanov@gmail.com>
2010-11-16 10:53:52 +0600

    Pagination factory

---
+++ spec/spec_helpers/fixtures/pagination.rb
@@ -0,0 +1,6 @@
+Factory.define :pagination, :class => SData::Collection::Pagination do |pagination|
+  pagination.default_items_per_page 10
+  pagination.maximum_items_per_page 100
+  pagination.startIndex 1
+  pagination.count 100
+end
