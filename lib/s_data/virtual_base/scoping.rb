e83c554caf00317c7e19bf976a71895072e84ca0
DanielVartanov <daniel.vartanov@gmail.com>
2010-11-18 16:55:52 +0600

    Changed chain of nested blocks to chain of scoping methods calls

--- lib/s_data/virtual_base/scoping.rb
+++ lib/s_data/virtual_base/scoping.rb
@@ -1,43 +1,27 @@
 module SData
   module Resource
     module Scoping
-      def with_pagination(pagination, &block)
-        if pagination.zero_based_start_index.nil?
-          yield self
-        else
-          options = {:offset => pagination.zero_based_start_index, :limit => pagination.records_to_return}
-          self.baze_class.with_scope(:find => options) do
-            yield self
-          end
-        end
+      def with_pagination(pagination)
+        scoped(:offset => pagination.zero_based_start_index, :limit => pagination.records_to_return)
       end
 
-      def with_conditions(conditions, &block)
-        options = {:conditions => conditions}
-        self.baze_class.with_scope(:find => options) do
-          yield self
-        end      
+      def with_conditions(conditions)
+        scoped(:conditions => conditions)
       end
-      
-      def with_predicate(raw_predicate, &block)
-        if raw_predicate.nil?
-          yield self
-        else
-          predicate = SData::Predicate.parse(self.payload_map.baze_fields, raw_predicate)
-          yield self.with_conditions(predicate.to_conditions, &block)
-        end        
+
+      def with_predicate(raw_predicate)
+        predicate = SData::Predicate.parse(self.payload_map.baze_fields, raw_predicate)
+        with_conditions(predicate.to_conditions)
       end
-      
+
       # TODO: rename bb_model_id and bb_model_type to model_id and model_type
       def with_linking(linking, uuid=nil)
         if linking
           uuid_clause = uuid.nil? ? '' : "uuid = '#{Predicate.strip_quotes(uuid)}' and "
           tablename = self.baze_class_name.tableize
-          with_conditions("#{tablename}.id IN (SELECT bb_model_id FROM sd_uuids WHERE #{uuid_clause}(bb_model_type = '#{baze_class_name}') and (sd_class = '#{sdata_name}'))") do
-            yield self
-          end
+          with_conditions("#{tablename}.id IN (SELECT bb_model_id FROM sd_uuids WHERE #{uuid_clause}(bb_model_type = '#{baze_class_name}') and (sd_class = '#{sdata_name}'))")
         else
-          yield self
+          self
         end
       end
       
@@ -61,6 +45,10 @@ module SData
         old_baze_class, self.baze_class = self.baze_class, class_or_scope
         yield
         self.baze_class = old_baze_class
+      end
+
+      def scoped(options)
+        ActiveRecord::NamedScope::Scope.new(self.baze_class, options)
       end
     end
   end
