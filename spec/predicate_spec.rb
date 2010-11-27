ff4618b5955baca4316efbcd0e7cc0b42c6737c4
DanielVartanov <daniel.vartanov@gmail.com>
2010-11-27 21:34:40 +0600

    Reduced variable scope in spec (converted ivar into local var)

--- spec/predicate_spec.rb
+++ spec/predicate_spec.rb
@@ -5,46 +5,46 @@ include SData
 describe Predicate do
   context "when predicate string is valid" do
     it "should convert given field name from hungarian notation to underscore notation" do
-      @predicate = Predicate.parse({ :born_at => :born_at }, "bornAt gt 1900")
+      predicate = Predicate.parse({ :born_at => :born_at }, "bornAt gt 1900")
       ConditionsBuilder.should_receive(:build_conditions).with(:born_at, :gt, '1900')
-      @predicate.to_conditions
+      predicate.to_conditions
     end
 
     it "should change field name according to a given map" do
-      @predicate = Predicate.parse({ :born_at => :crafted_at }, "bornAt gt 1900")
+      predicate = Predicate.parse({ :born_at => :crafted_at }, "bornAt gt 1900")
       ConditionsBuilder.should_receive(:build_conditions).with(:crafted_at, :gt, '1900')
-      @predicate.to_conditions
+      predicate.to_conditions
     end
 
     it "should parse predicate string correctly" do
-      @predicate = Predicate.parse({ :born_at => :born_at }, "bornAt gt 1900")
+      predicate = Predicate.parse({ :born_at => :born_at }, "bornAt gt 1900")
       ConditionsBuilder.should_receive(:build_conditions).with(:born_at, :gt, '1900')
-      @predicate.to_conditions
+      predicate.to_conditions
     end
 
     it "should strip wrapper quote marks" do
-      @predicate = Predicate.parse({ :born_at => :born_at }, "bornAt gt '1900'")
+      predicate = Predicate.parse({ :born_at => :born_at }, "bornAt gt '1900'")
       ConditionsBuilder.should_receive(:build_conditions).with(:born_at, :gt, '1900')
-      @predicate.to_conditions
+      predicate.to_conditions
     end
     
     it "should accept non-alphanumeric characters as part of the value" do
-      @predicate = Predicate.parse({ :born_at => :born_at }, "bornAt gt '1`~!@\#$%^&*()_-+={[}]\|'\";:900'")
+      predicate = Predicate.parse({ :born_at => :born_at }, "bornAt gt '1`~!@\#$%^&*()_-+={[}]\|'\";:900'")
       ConditionsBuilder.should_receive(:build_conditions).with(:born_at, :gt, "1`~!@\#$%^&*()_-+={[}]\|'\";:900")
-      @predicate.to_conditions
+      predicate.to_conditions
     end
 
     it "should accept quote-marked empty string" do
-      @predicate = Predicate.parse({ :born_at => :born_at }, "bornAt gt ''")
+      predicate = Predicate.parse({ :born_at => :born_at }, "bornAt gt ''")
       ConditionsBuilder.should_receive(:build_conditions).with(:born_at, :gt, "")
-      @predicate.to_conditions
+      predicate.to_conditions
     end
 
     context "when condition contains 'ne' relation" do
       it "should parse it correctly" do
-        @predicate = Predicate.parse({ :born_at => :born_at }, "bornAt ne 1900")
+        predicate = Predicate.parse({ :born_at => :born_at }, "bornAt ne 1900")
         ConditionsBuilder.should_receive(:build_conditions).with(:born_at, :ne, "1900")
-        @predicate.to_conditions
+        predicate.to_conditions
       end
     end
   end
