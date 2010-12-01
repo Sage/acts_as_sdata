3147cb3c4dad6c6d41b243319c0f20103ecc64e6
DanielVartanov <daniel.vartanov@gmail.com>
2010-12-01 15:03:08 +0600

    Introducing SData::Resource::Scope. Fully functioning right now.

---
+++ lib/s_data/resource/scope.rb
@@ -0,0 +1,45 @@
+module SData
+  module Resource
+    class Scope < Struct.new(:resource_class, :baze_scope)
+      def all(*params)
+        resource_class.collection(baze_scope.all(*params))
+      end
+
+      def with_pagination(pagination, &block)
+        yield scoped(:offset => pagination.zero_based_start_index, :limit => pagination.records_to_return)
+      end
+
+      def with_conditions(conditions, &block)
+        yield scoped(:conditions => conditions)
+      end
+
+      def with_predicate(raw_predicate, &block)
+        if raw_predicate.nil?
+          yield self
+        else
+          predicate = SData::Predicate.parse(resource_class.payload_map.baze_fields, raw_predicate)
+          with_conditions(predicate.to_conditions, &block)
+        end
+      end
+
+      def scoped(conditions)
+        clone_with_baze_scope(baze_scope.scoped(conditions))
+      end
+
+      def clone_with_baze_scope(new_scope)
+        self.class.new(resource_class, new_scope)
+      end
+
+      # TODO: rename bb_model_id and bb_model_type to model_id and model_type
+      def with_linking(linking, uuid=nil, &block)
+        if linking
+          uuid_clause = uuid.nil? ? '' : "uuid = '#{Predicate.strip_quotes(uuid)}' and "
+          tablename = resource_class.baze_class_name.tableize
+          with_conditions("#{tablename}.id IN (SELECT bb_model_id FROM sd_uuids WHERE #{uuid_clause}(bb_model_type = '#{baze_class_name}') and (sd_class = '#{sdata_name}'))", &block)
+        else
+          yield self
+        end
+      end
+    end
+  end
+end
--- lib/s_data/resource/scoping.rb
+++ lib/s_data/resource/scoping.rb
@@ -1,58 +1,19 @@
 module SData
   module Resource
     module Scoping
-      def with_pagination(pagination, &block)
-        yield scoped(:offset => pagination.zero_based_start_index, :limit => pagination.records_to_return)
-      end
-
-      def with_conditions(conditions, &block)
-        yield scoped(:conditions => conditions)
-      end
-
-      def with_predicate(raw_predicate, &block)
-        if raw_predicate.nil?
-          yield self
-        else
-          predicate = SData::Predicate.parse(self.payload_map.baze_fields, raw_predicate)
-          with_conditions(predicate.to_conditions, &block)
-        end
-      end
-
-      # TODO: rename bb_model_id and bb_model_type to model_id and model_type
-      def with_linking(linking, uuid=nil, &block)
-        if linking
-          uuid_clause = uuid.nil? ? '' : "uuid = '#{Predicate.strip_quotes(uuid)}' and "
-          tablename = self.baze_class_name.tableize
-          with_conditions("#{tablename}.id IN (SELECT bb_model_id FROM sd_uuids WHERE #{uuid_clause}(bb_model_type = '#{baze_class_name}') and (sd_class = '#{sdata_name}'))", &block)
-        else
-          yield self
-        end
-      end
-
       def sdata_scope_for_context(context)
-        has_sdata_scope = self.baze_class.respond_to?(:sdata_scope_for_context)
         if SData.enforce_scoping?
-          unless has_sdata_scope
+          unless has_sdata_scope?
             raise SData::Exceptions::VirtualBase::MissingScope.new("#{self.name}: missing sdata_scope_for_context for #{self.baze_class.name}")
           end
         end
-        scope = has_sdata_scope ? self.baze_class.sdata_scope_for_context(context) : self.baze_class
-        ScopedVirtualBase.new(scope, self)
+        
+        baze_scope = has_sdata_scope? ? self.baze_class.sdata_scope_for_context(context) : self.baze_class
+        SData::Resource::Scope.new(self, baze_scope)
       end
 
-      def with_baze(active_record_instance, &block)
-        self.with_baze_class(active_record_instance.class, &block)
-      end
-      
-      # RADAR: not threadsafe
-      def with_baze_class(class_or_scope)
-        old_baze_class, self.baze_class = self.baze_class, class_or_scope
-        yield
-        self.baze_class = old_baze_class
-      end
-
-      def scoped(options)
-        self.baze_class.scoped(options)
+      def has_sdata_scope?
+        self.baze_class.respond_to?(:sdata_scope_for_context)
       end
     end
   end
