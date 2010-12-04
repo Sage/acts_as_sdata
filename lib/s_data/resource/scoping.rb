239e7732bdf6ae609db431681af6a8567d99db87
DanielVartanov <daniel.vartanov@gmail.com>
2010-12-04 15:02:42 +0600

    sdata_scope_for_context received directly a target user now

--- lib/s_data/resource/scoping.rb
+++ lib/s_data/resource/scoping.rb
@@ -1,14 +1,14 @@
 module SData
   module Resource
     module Scoping
-      def sdata_scope_for_context(context)
+      def sdata_scope_for_context(target_user)
         if SData.enforce_scoping?
           unless has_sdata_scope?
             raise SData::Exceptions::VirtualBase::MissingScope.new("#{self.name}: missing sdata_scope_for_context for #{self.baze_class.name}")
           end
         end
         
-        baze_scope = has_sdata_scope? ? self.baze_class.sdata_scope_for_context(context) : self.baze_class
+        baze_scope = has_sdata_scope? ? self.baze_class.sdata_scope_for_context(target_user) : self.baze_class
         SData::Resource::Scope.new(self, baze_scope)
       end
 
