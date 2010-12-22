06c27bbff68c045b3ef2c9967974e9a0c467e231
DanielVartanov <daniel.vartanov@gmail.com>
2010-12-22 14:42:30 +0600

    Slightly refactored Links. Now it is at least readable.

--- lib/s_data/collection/links.rb
+++ lib/s_data/collection/links.rb
@@ -7,39 +7,53 @@ module SData
         one_based_start_index = pagination.one_based_start_index
         zero_based_start_index = pagination.zero_based_start_index
 
-        atom_links= Atom::Link.new(
-                                     :rel => 'self',
-                                     :href => (base_url + "?#{query_params.to_param}".chomp('?')),
+        atom_links = [Atom::Link.new(:rel => 'self',
+                                     :href => current_url,
                                      :type => 'application/atom+xml; type=feed',
-                                     :title => 'Refresh')
+                                     :title => 'Refresh')]
+        
         if (records_to_return > 0) && (total_results > records_to_return)
           atom_links << Atom::Link.new(
                                        :rel => 'first',
-                                       :href => (base_url + "?#{query_params.merge(:startIndex => '1').to_param}"),
+                                       :href => current_url_with_added_params(:startIndex => '1'),
                                        :type => 'application/atom+xml; type=feed',
                                        :title => 'First Page')
           atom_links << Atom::Link.new(
                                        :rel => 'last',
-                                       :href => (base_url + "?#{query_params.merge(:startIndex => [1,(@last=(((total_results-zero_based_start_index - 1) / records_to_return * records_to_return) + zero_based_start_index + 1))].max).to_param}"),
+                                       :href => current_url_with_added_params(:startIndex => [1,(@last=(((total_results-zero_based_start_index - 1) / records_to_return * records_to_return) + zero_based_start_index + 1))].max),
                                        :type => 'application/atom+xml; type=feed',
                                        :title => 'Last Page')
           if (one_based_start_index+records_to_return) <= total_results
             atom_links << Atom::Link.new(
                                          :rel => 'next',
-                                         :href => (base_url + "?#{query_params.merge(:startIndex => [1,[@last, (one_based_start_index+records_to_return)].min].max.to_s).to_param}"),
+                                         :href => current_url_with_added_params(:startIndex => [1,[@last, (one_based_start_index+records_to_return)].min].max),
                                          :type => 'application/atom+xml; type=feed',
                                          :title => 'Next Page')
           end
           if (one_based_start_index > 1)
             atom_links << Atom::Link.new(
                                          :rel => 'previous',
-                                         :href => (base_url + "?#{query_params.merge(:startIndex => [1,[@last, (one_based_start_index-records_to_return)].min].max.to_s).to_param}"),
+                                         :href => current_url_with_added_params(:startIndex => [1,[@last, (one_based_start_index-records_to_return)].min].max),
                                          :type => 'application/atom+xml; type=feed',
                                          :title => 'Previous Page')
           end
-
-          atom_links
         end
+        
+        atom_links
+      end
+
+      protected
+
+      def current_url_with_added_params(added_params)
+        compose_url(query_params.merge(added_params))
+      end
+
+      def current_url
+        compose_url(query_params)
+      end
+
+      def compose_url(params)
+        base_url + "?#{params.to_param}"
       end
     end
   end
