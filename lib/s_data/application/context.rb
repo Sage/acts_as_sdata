50b22602ec9c71fb2473e4afd11852d900e7a1ef
DanielVartanov <daniel.vartanov@gmail.com>
2010-12-23 12:25:22 +0600

    Introducing SData::Application::Context

---
+++ lib/s_data/application/context.rb
@@ -0,0 +1,29 @@
+module SData
+  module Application
+    class Context < Struct.new(:params, :query_params)
+      def sync?
+        params[:sync].to_s == 'true'
+      end
+
+      def dataset
+        params[:dataset]
+      end
+
+      def selected
+        params[:select].to_s.split(',')
+      end
+
+      def included
+        params[:include].to_s.split(',')
+      end
+
+      def maximum_precedence
+        params[:precedence].blank? ? params[:precedence].to_i : 100
+      end
+
+      def expand?
+        (sync || included.include?('$children')) ? :all_children : :immediate_children
+      end
+    end
+  end
+end
