065efe1778f3080541ee2c94cd9658252108db3e
DanielVartanov <daniel.vartanov@gmail.com>
2011-01-08 16:29:04 +0600

    Fixed Pagination initialization method for the sake of FactoryGirl

--- lib/s_data/collection/pagination.rb
+++ lib/s_data/collection/pagination.rb
@@ -1,13 +1,12 @@
 module SData
   class Collection
-    class Pagination
-      attr_accessor :default_items_per_page, :maximum_items_per_page, :start_index, :count
+    class Pagination < Struct.new(:default_items_per_page, :maximum_items_per_page, :start_index, :count)
       
-      def initialize(default_items_per_page, maximum_items_per_page, start_index, count)
+      def initialize(*args)
+        super(*args)
+        
         self.default_items_per_page = default_items_per_page || 10
         self.maximum_items_per_page = maximum_items_per_page || 100
-        self.count = count
-        self.start_index = start_index
       end
       
       def records_to_return
