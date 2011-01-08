52bcef28f7d6beae4545581dc8466d2a38568329
DanielVartanov <daniel.vartanov@gmail.com>
2011-01-08 16:28:38 +0600

    RSpecFriendlyEqualityOperator is now included into other Nokogiri classes

--- spec/spec_helpers/nokogiri_extensions/rspec_friendly_equality_operator.rb
+++ spec/spec_helpers/nokogiri_extensions/rspec_friendly_equality_operator.rb
@@ -1,4 +1,4 @@
-class Nokogiri::XML::NodeSet
+rspec_friendly_equality_operator = lambda do
   def ==(other)
     if other.is_a?(String)
       self.to_s == other
@@ -6,4 +6,8 @@ class Nokogiri::XML::NodeSet
       super(other)
     end
   end
+end
+
+[Nokogiri::XML::NodeSet, Nokogiri::XML::Text, Nokogiri::XML::Attr].each do |klass|
+  klass.class_eval &rspec_friendly_equality_operator
 end
