d785252feb0baa42c47ff9c568462ad25612270f
daniel <daniel@daniel-lenovo.(none)>
2011-02-20 22:49:00 +0600

    Fixed factory for PaginationParams (curly brackets were interpreted as a block, not as an empty hash)

--- spec/spec_helpers/fixtures/pagination_params.rb
+++ spec/spec_helpers/fixtures/pagination_params.rb
@@ -1,4 +1,4 @@
 Factory.define :pagination_params, :class => SData::Collection::PaginationParams do |pagination_params|
   pagination_params.feed_options :default_items_per_page => 10, :maximum_items_per_page => 100
-  pagination_params.params {}
+  pagination_params.params Hash.new
 end
