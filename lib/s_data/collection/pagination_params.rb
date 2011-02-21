a1ac3cfc308dd3577ca1150d5840b30b17b8d371
DanielVartanov <daniel.vartanov@gmail.com>
2011-02-21 13:59:20 +0600

    All specs for PaginationParams are satisfied

--- lib/s_data/collection/pagination_params.rb
+++ lib/s_data/collection/pagination_params.rb
@@ -13,16 +13,32 @@ module SData
         items_per_page
       end
 
-      def count
-        @count ||= (params[:count].to_i.to_s == params[:count]) ? params[:count].to_i : default_items_per_page
-      end
-
       def default_items_per_page
         feed_options[:default_items_per_page] || 10
       end
 
       def maximum_items_per_page
         feed_options[:maximum_items_per_page] || 100
+      end
+
+      def count
+        @count ||= extract_number_from_params(:count, default_items_per_page)
+      end
+
+      def start_index
+        [extract_number_from_params(:startIndex, 1), 1].max
+      end
+
+      alias :one_based_start_index :start_index
+
+      def zero_based_start_index
+        start_index - 1
+      end
+
+      protected
+
+      def extract_number_from_params(key, default_value)
+        result = (params[key].to_i.to_s == params[key]) ? params[key].to_i : default_value
       end
     end
   end
