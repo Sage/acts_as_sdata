2502fcb073bedb8f486e4b20a603ecd7b57d5895
DanielVartanov <daniel.vartanov@gmail.com>
2011-02-21 17:23:33 +0600

    Fixed some specs for Pagination. Added specs for Pagination#page_count and Pagination#current_page

--- spec/collection/pagination_spec.rb
+++ spec/collection/pagination_spec.rb
@@ -1,9 +1,9 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe SData::Collection::Pagination do
-  subject { @pagination.single_page? }
-  
   describe "#single_page?" do
+    subject { @pagination.single_page? }
+    
     context "when entry count is greater than items per page" do
       before { @pagination = Factory.build(:pagination, :entry_count => 12) }
       
@@ -22,8 +22,8 @@ describe SData::Collection::Pagination do
     
     context "when start index belongs to first page" do
       before do
-        pagination_params = Factory.build :pagination_params, :params => { :start_index => 10 }
-        @pagination = Factory.build :pagination, :pagination_params => pagination_params
+        pagination_params = Factory.build :pagination_params, :params => { :startIndex => 10 }
+        @pagination = Factory.build :pagination, :pagination_params => pagination_params, :entry_count => 12
       end
      
       it { should be_true }
@@ -31,8 +31,8 @@ describe SData::Collection::Pagination do
 
     context "when start index is out of first page bounds" do
       before do
-        pagination_params = Factory.build :pagination_params, :params => { :start_index => 11 }
-        @pagination = Factory.build :pagination, :pagination_params => pagination_params
+        pagination_params = Factory.build :pagination_params, :params => { :startIndex => 11 }
+        @pagination = Factory.build :pagination, :pagination_params => pagination_params, :entry_count => 12
       end
       
       it { should be_false }
@@ -44,7 +44,7 @@ describe SData::Collection::Pagination do
     
     context "when start index belongs to last page" do
       before do
-        pagination_params = Factory.build :pagination_params, :params => { :start_index => 11 }
+        pagination_params = Factory.build :pagination_params, :params => { :startIndex => 11 }
         @pagination = Factory.build :pagination, :pagination_params => pagination_params, :entry_count => 12
       end
      
@@ -53,7 +53,7 @@ describe SData::Collection::Pagination do
 
     context "when start index is out of last page bounds" do
       before do
-        pagination_params = Factory.build :pagination_params, :params => { :start_index => 9 }
+        pagination_params = Factory.build :pagination_params, :params => { :startIndex => 9 }
         @pagination = Factory.build :pagination, :pagination_params => pagination_params, :entry_count => 12
       end
       
@@ -62,26 +62,15 @@ describe SData::Collection::Pagination do
   end
 
   describe "#current_page_start_index" do
+    before do
+      pagination_params = Factory.build :pagination_params, :params => { :startIndex => 15 }
+      @pagination = Factory.build :pagination, :pagination_params => pagination_params, :entry_count => 22
+    end
+    
     subject { @pagination.current_page_start_index }
 
-    context "when startIndex param is provided by user" do
-      before do
-        pagination_params = Factory.build :pagination_params, :params => { :start_index => 5 }
-        @pagination = Factory.build :pagination, :pagination_params => pagination_params
-      end
-
-      it "should return one-based current start index" do
-        should == 5
-      end
-    end
-
-    context "when startIndex is not provided" do
-      before do
-        pagination_params = Factory.build :pagination_params, :params => {}
-        @pagination = Factory.build :pagination, :pagination_params => pagination_params
-      end
-
-      it { should == 1 }
+    it "should return beginning of the current page" do
+      should == 11
     end
   end
 
@@ -100,7 +89,7 @@ describe SData::Collection::Pagination do
       before { @pagination = Factory.build :pagination, :entry_count => 20 }
 
       it "should return beginning of the last page" do
-        should == 10
+        should == 11
       end
     end
 
@@ -108,14 +97,14 @@ describe SData::Collection::Pagination do
       before { @pagination = Factory.build :pagination, :entry_count => 23 }
 
       it "should leave aliquant remainder on the last page" do
-        should == 20
+        should == 21
       end
     end
   end
 
   context "when current start index is in the beginning of page" do
     before do
-      pagination_params = Factory.build :pagination_params, :params => { :start_index => 21 }
+      pagination_params = Factory.build :pagination_params, :params => { :startIndex => 21 }
       @pagination = Factory.build :pagination, :pagination_params => pagination_params, :entry_count => 44
     end
 
@@ -138,7 +127,7 @@ describe SData::Collection::Pagination do
 
   context "when current start index is in the middle of page" do
     before do
-      pagination_params = Factory.build :pagination_params, :params => { :start_index => 27 }
+      pagination_params = Factory.build :pagination_params, :params => { :startIndex => 27 }
       @pagination = Factory.build :pagination, :pagination_params => pagination_params, :entry_count => 42
     end
 
@@ -156,6 +145,64 @@ describe SData::Collection::Pagination do
       it "should still return beginning of next page" do
         should == 31
       end
+    end
+  end
+
+  describe "#page_count" do
+    subject { @pagination.page_count }
+    
+    context "when :count is a multiple of actual entry count" do
+      before { @pagination = Factory.build :pagination, :entry_count => 30 }
+
+      it { should == 3 }
+    end
+
+    context "when :count is not a multiple of actual entry count" do
+      before { @pagination = Factory.build :pagination, :entry_count => 39 }
+
+      it { should == 4 }
+    end
+    
+    context "when :count parm is also provided" do
+      before do
+        pagination_params = Factory.build :pagination_params, :params => { :count => 13 }
+        @pagination = Factory.build :pagination, :pagination_params => pagination_params, :entry_count => 39
+      end
+      
+      it "should be taken into account" do
+        should == 3
+      end
+    end
+  end
+
+  describe "#current_page" do
+    subject { @pagination.current_page }
+    
+    context "when :startIndex is at the beginning of a page" do
+      before do
+        pagination_params = Factory.build :pagination_params, :params => { :startIndex => 11 }
+        @pagination = Factory.build :pagination, :pagination_params => pagination_params, :entry_count => 22
+      end
+
+      it { should == 2 }
+    end
+    
+    context "when :startIndex is at the end of a page" do
+      before do
+        pagination_params = Factory.build :pagination_params, :params => { :startIndex => 10 }
+        @pagination = Factory.build :pagination, :pagination_params => pagination_params, :entry_count => 22
+      end
+
+      it { should == 1 }
+    end
+    
+    context "when :startIndex is in the middle of a page" do
+      before do
+        pagination_params = Factory.build :pagination_params, :params => { :startIndex => 22 }
+        @pagination = Factory.build :pagination, :pagination_params => pagination_params, :entry_count => 22
+      end
+
+      it { should == 3 }
     end
   end
 end
