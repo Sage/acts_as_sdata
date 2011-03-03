6fff1d3c80153a8dfc633db6fb799b7f3cdd0582
DanielVartanov <daniel.vartanov@gmail.com>
2011-03-03 18:40:20 +0600

    UrlComposer is refactored

--- lib/s_data/collection/links/url_composer.rb
+++ lib/s_data/collection/links/url_composer.rb
@@ -6,17 +6,24 @@ module SData
           params = (query_params || {}).dup
           params[:startIndex] = startIndex
 
+          remove_default_values(params)
+          query_string = compose_query_string(params)
+
+          base_url + query_string
+        end
+
+        protected
+        
+        def remove_default_values(params)
           params.delete :startIndex if params[:startIndex] == 1
           params.delete :count if params[:count] == 10
+        end
 
-          unless params.empty?
-            pair_strings = params.map { |pair| pair.join('=') }
-            query_string = "?" + pair_strings.join('&')
-          else
-            query_string = ""
-          end
+        def compose_query_string(params)
+          return "" if params.empty?
           
-          base_url + query_string
+          pair_strings = params.map { |pair| pair.join('=') }
+          "?" + pair_strings.join('&')
         end
       end
     end
