7d4ca902f2ba6d3e5dd0d62c0560231e424be1a8
daniel <daniel@daniel-lenovo.(none)>
2011-03-06 17:27:26 +0600

    A lot of changes in to_atom_spec.rb. Making it respect the new DSL

--- spec/resource/to_atom_spec.rb
+++ spec/resource/to_atom_spec.rb
@@ -3,80 +3,68 @@ require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 describe SData::Resource::Base, "#to_atom" do
   describe "given a Resource" do
     before :all do
-      class Base < SData::Resource::Base; end
+      remove_constants :Base
+
+      module SData; module Contracts; module CrmErp; end; end; end;
+      class SData::Contracts::CrmErp::Base < SData::Resource::Base;
+        define_payload_map Hash.new
+      end
+
+      @context = SData::Application::Context.new({ :dataset => '-' }, Hash.new)
     end
 
     describe "when there is no sdata options" do
       before :each do
-        Base.class_eval { has_sdata_options Hash.new }
-        @model = Base.new
+        SData::Contracts::CrmErp::Base.class_eval { has_sdata_options Hash.new }
+        @model = SData::Contracts::CrmErp::Base.new
         @model.stub! :id => 1, :name => 'John Smith', :updated_at => Time.now - 1.day, :created_by => @model, :sage_username => 'basic_user' 
         @model.stub! :sdata_content => "Base ##{@model.id}: #{@model.name}", :attributes => {}
       end
 
       it "should return an Atom::Entry instance" do
-        @model.to_atom(:dataset => '-').should be_kind_of(Atom::Entry)
+        @model.to_atom(@context).should be_kind_of(Atom::Entry)
       end
 
       it "should assign model name to Atom::Entry#content" do
-        @model.to_atom(:dataset => '-').content.should == 'Base #1: John Smith'
+        @model.to_atom(@context).content.should == 'Base #1: John Smith'
       end
 
       it "should assign model name and id to Atom::Entry#title" do
-        @model.to_atom(:dataset => '-').title.should == 'Base 1'
+        @model.to_atom(@context).title.should == 'Base 1'
       end
 
       it "should assign Atom::Entry#updated" do
-        Time.parse(@model.to_atom(:dataset => '-').updated).should < Time.now-1.day
-        Time.parse(@model.to_atom(:dataset => '-').updated).should > Time.now-1.day-1.minute        
-      end
-
-      it "should assign Atom::Entry#links" do
-        @model.to_atom(:dataset => '-').links.size.should == 1
-        @model.to_atom(:dataset => '-').links[0].rel.should == 'self'
-        @model.to_atom(:dataset => '-').links[0].href.should == "http://www.example.com/sdata/example/myContract/-/bases('1')"
-      end
-
-      it "should assign Atom::Entry#links when param query is present" do
-        @model.to_atom(:dataset => '-', :select => 'attribute').links.size.should == 1
-        @model.to_atom(:dataset => '-', :select => 'attribute').links[0].rel.should == 'self'
-        @model.to_atom(:dataset => '-', :select => 'attribute').links[0].href.should == "http://www.example.com/sdata/example/myContract/-/bases('1')?select=attribute"
+        Time.parse(@model.to_atom(@context).updated).should < Time.now-1.day
+        Time.parse(@model.to_atom(@context).updated).should > Time.now-1.day-1.minute        
       end
 
       it "should assign Atom::Entry::id" do
-        @model.to_atom(:dataset => '-').id.should == "http://www.example.com/sdata/example/myContract/-/bases('1')"
+        @model.to_atom(@context).id.should == "http://www.example.com/sdata/example/myContract/-/bases('1')"
       end
 
       it "should assign Atom::Entry::categories" do
-        @model.to_atom.categories.size.should == 1
-        @model.to_atom.categories[0].term.should == "base"
-        @model.to_atom.categories[0].label.should == "Base"
-        @model.to_atom.categories[0].scheme.should == "http://schemas.sage.com/sdata/categories"
+        @model.to_atom(@context).categories.size.should == 1
+        @model.to_atom(@context).categories[0].term.should == "base"
+        @model.to_atom(@context).categories[0].label.should == "Base"
+        @model.to_atom(@context).categories[0].scheme.should == "http://schemas.sage.com/sdata/categories"
       end
-
-#      #QUESTION: Why is it commented out?
-#      it "should expose activerecord attributes in a simple XML extension" do
-#        @model.stub! :attributes => { :last_name => "Washington", :first_name => "George" }
-#        atom_entry = @model.to_atom
-#        atom_entry['http://sdata.sage.com/schemes/attributes'].should == { 'last_name' => ["Washington"], 'first_name' => ["George"] }
-#      end
     end
 
     describe "when there are sdata_options" do
       before :each do
-        Base.class_eval do
+        SData::Contracts::CrmErp::Base.class_eval do
           has_sdata_options :title => lambda { "#{id}: #{name}" },
                             :content => lambda { "#{name}" }
         end
 
-        @model = Base.new
+        @model = SData::Contracts::CrmErp::Base.new
         @model.stub! :id => 1, :name => 'Test', :updated_at => Time.now - 1.day, :created_by => @model, :sage_username => 'basic_user'
         @model.stub! :sdata_content => "Base ##{@model.id}: #{@model.name}",  :to_xml => ''
       end
 
       it "should evaulate given lambda's in the correct context" do
-        @model.to_atom.title.should == '1: Test'
-        @model.to_atom.content.should == 'Base #1: Test'
+        @model.to_atom(@context).title.should == '1: Test'
+        @model.to_atom(@context).content.should == 'Base #1: Test'
       end
     end
   end
